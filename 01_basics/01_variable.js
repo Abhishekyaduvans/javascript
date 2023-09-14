44431e3c4e82e29406b0daf26262080ad80ecced
Abhishekyaduvans <93341638+Abhishekyaduvans@users.noreply.github.com>
2023-09-14 19:03:30 +0000

    config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=12143432
+let acountemail="abhis@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur" 
+let accountstatus;
+
+
+// accountId=2 not allowed 
+
+accountemail="heru@gmail.com"
+accountpassword="47y9823"
+accountcity="varanasi"
+console.log(accountId);
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/ 
+console.table([accountemail,accountpassword,accountcity,accountstatus]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+// alert(3+3) we are using node js not browser
+ console.log(3
+    +3)//code readability should be high
+console.log("hitesh")
+
+
+let name="abhki!"
+let age=18
+let islogged =true
+let state;
+//number =>2 to power 53
+//big int
+//string=""
+//null= standalone value
+//undefined =
+//symbol=uniqueness
+console.log(typeof (age) );//number
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
