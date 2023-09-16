8ddd11ef898c50e8c37d064c6b36ad53e7ecb8e9
Abhishekyaduvans <93341638+Abhishekyaduvans@users.noreply.github.com>
2023-09-16 17:26:41 +0000

    config done

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,12 @@
+let score = undefined
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+
+console.log(valueInNumber)
+
+//"33"=> 33
+//"33abc"=>NaN(not a number)
+//true=> 1; false=>0
